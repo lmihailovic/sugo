4bf43d4ccf1449a6a2b0772ae76717ad1083bb83
Luka Mihailović <miha.luka1006@gmail.com>
2025-08-20 00:01:42 +0200

    handle parsing content front matter

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"os"
+	"strings"
 	"text/template"
 
 	"github.com/yuin/goldmark"
@@ -13,32 +16,67 @@ type Blogpost struct {
 	Content string
 }
 
+func GetFrontMatter(filePath string, delimiter string) (map[string]string, int, error) {
+	fileBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, -1, err
+	}
+
+	fileContent := string(fileBytes)
+
+	startIndex := strings.Index(fileContent, delimiter)
+	if startIndex == -1 {
+		return nil, 1, errors.New("no front matter delimiter found in content file")
+	}
+	startIndex += len(delimiter)
+
+	endIndex := strings.Index(fileContent[startIndex:], delimiter)
+	endIndex += len(delimiter)
+
+	frontMatter := "{" + fileContent[startIndex:endIndex] + "}\n"
+
+	if !json.Valid([]byte(frontMatter)) {
+		return nil, -1, errors.New("invalid json in front matter")
+	}
+
+	data := make(map[string]string, 0)
+
+	err = json.Unmarshal([]byte(frontMatter), &data)
+	if err != nil {
+		return nil, endIndex + len(delimiter), err
+	}
+
+	return data, endIndex + len(delimiter), nil
+}
+
 func main() {
+	contentPath := "content/blog/smth.md"
+
 	tmpl, err := template.ParseFiles("templates/blogpost.html")
 	if err != nil {
 		panic(err)
 	}
 
-	blogpost := Blogpost{"Prvi clanak", ""}
-
-	// err = tmpl.Execute(os.Stdout, blogpost)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	frontMatter, fmEndIndex, err := GetFrontMatter(contentPath, "+++")
+	if err != nil {
+		panic(err)
+	}
 
 	var buf bytes.Buffer
 
-	file, err := os.ReadFile("content/blog/darkmode-difficulties.md")
+	fileBytes, err := os.ReadFile(contentPath)
 	if err != nil {
 		panic(err)
 	}
 
-	err = goldmark.Convert(file, &buf)
+	fileContent := string(fileBytes)[fmEndIndex:]
+
+	err = goldmark.Convert([]byte(fileContent), &buf)
 	if err != nil {
 		panic(err)
 	}
 
-	blogpost.Content = buf.String()
+	blogpost := Blogpost{frontMatter["Title"], buf.String()}
 
 	err = tmpl.Execute(os.Stdout, blogpost)
 	if err != nil {
